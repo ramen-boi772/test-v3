d417ea0dc84d3941d3919b893851209cda265baf
ramen-boi772 <3001890@stu.bisd.us>
2026-02-24 12:38:24 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.use(cors());
 
 // DuckDuckGo Instant Answer API endpoint
